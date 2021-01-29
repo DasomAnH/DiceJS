0fe54a09772e81852cc91e30c0c0f75992a46f54
DasomAnH <ektha116@gmail.com>
2021-01-29 14:13:34 -0500

    diceGame

---
+++ index.js
@@ -0,0 +1,9 @@
+const randomNum1 = Math.floor(Math.random() * 6) + 1;
+
+const randomDiceImage = 'dice' + randomNum1 + '.png';
+
+const randomImage;
+
+document.querySelectorAll;
+
+const randomNum2 = Math.floor(Math.random() * 6) + 1;
